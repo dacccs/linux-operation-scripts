403c3688f429a902896dbf787ffc2edb58da4c0a
dacccs <dacccs@gmail.com>
2025-02-12 11:48:40 +0100

    new printout format

--- cpu-info.sh
+++ cpu-info.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 ################################################
 # CPU Info Script                              #
-# Created: dacccs                              #
-# Last modification: 2021.04.12                #
+# Created: Ottó Király                         #
+# Last modification: 2025.02.12                #
 ################################################
-VERSION=1.0043;
+VERSION=1.0044;
+
+function printout() {
+	printf "    %-28s: %-s \n" "$1" "$2";
+}
+
 cpu=$(dmidecode -t processor | sed -e 's/[ \t]*//' | sed -e 's/$/#/');
 cpu_all=$(echo $cpu | sed -e 's/#/\n/g' | sed -e 's/^ //g' | grep -iE "^Handle" | wc -l);
 cpu_inst=$(echo $cpu | sed -e 's/#/\n/g' | sed -e 's/^ //g' | grep -i " popu" | wc -l);
@@ -13,16 +18,17 @@ cpu_core=$(echo $cpu | sed -e 's/#/\n/g' | sed -e 's/^ //g' | grep -i "core c" |
 cpu_thread=$(echo $cpu | sed -e 's/#/\n/g' | sed -e 's/^ //g' | grep -i "thread c" | sort -u | cut -d' ' -f3)
 
 echo -e '--CPU--------------------------------------------------------------------------';
+echo -e '';
 
-#echo -e '\tCPU(s):\t\t\t'$cpu_inst" x "$cpu_type;
-#let cpu_inst=cpu_all-cpu_inst;
-echo -e '\tAll/Free socket(s):\t'$cpu_all'/'$(($cpu_all-$cpu_inst));
-echo -e '\tTotal Core Count:\t'$(( $cpu_inst*$cpu_core));
-echo -e '\tTotal Thread Count:\t'$(( $cpu_inst*$cpu_thread))'\n';
-
-echo -e '\tModell:\t\t\t'$cpu_type;	
-echo -e '\tMax Speed:\t\t'$(echo $cpu | sed -e 's/#/\n/g' | sed -e 's/^ //g' | grep -i "Max Speed" | grep -iv unknow | sort -u | cut -d':' -f 2 | sed -e 's/^ //');
-echo -e '\tCurrent Speed:\t\t'$(echo $cpu | sed -e 's/#/\n/g' | sed -e 's/^ //g' | grep -i "Current Speed" | grep -iv unknow | sort -u | cut -d':' -f 2 | sed -e 's/^ //');
-echo -e '\tSocket:\t\t\t'$(echo $cpu | sed -e 's/#/\n/g' | sed -e 's/^ //g' | grep -i upgrade | sort -u | cut -d':' -f 2 | sed -e 's/^ //');
-echo -e '\tCPU Core Count:\t\t'$(echo $cpu | sed -e 's/#/\n/g' | sed -e 's/^ //g' | grep -i "core c" | sort -u | cut -d' ' -f3);
-echo -e '\tCPU Thread Count:\t'$(echo $cpu | sed -e 's/#/\n/g' | sed -e 's/^ //g' | grep -i "thread c" | sort -u | cut -d' ' -f3)'\n\n';
+printout 'All/Free socket(s)' "$cpu_all/$(($cpu_all-$cpu_inst))"
+printout 'Total Core Count' "$(( $cpu_inst*$cpu_core))";
+printout 'Total Thread Count' "$(( $cpu_inst*$cpu_thread))";
+echo -e '';
+		  
+printout 'Modell' "$cpu_type";
+printout 'Max Speed' "$(echo $cpu | sed -e 's/#/\n/g' | sed -e 's/^ //g' | grep -i 'Max Speed' | grep -iv unknow | sort -u | cut -d':' -f 2 | sed -e 's/^ //')";
+printout 'Current Speed' "$(echo $cpu | sed -e 's/#/\n/g' | sed -e 's/^ //g' | grep -i 'Current Speed' | grep -iv unknow | sort -u | cut -d':' -f 2 | sed -e 's/^ //')";
+printout 'Socket' "$(echo $cpu | sed -e 's/#/\n/g' | sed -e 's/^ //g' | grep -i upgrade | sort -u | cut -d':' -f 2 | sed -e 's/^ //')";
+printout 'CPU Core Count' "$(echo $cpu | sed -e 's/#/\n/g' | sed -e 's/^ //g' | grep -i 'core c' | sort -u | cut -d' ' -f3)";
+printout 'CPU Thread Count' "$(echo $cpu | sed -e 's/#/\n/g' | sed -e 's/^ //g' | grep -i 'thread c' | sort -u | cut -d' ' -f3)";
+echo -e '';
